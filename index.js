010562b3d32988d6c07424bee507b5d1d2986b5b
voste126 <steveaustine126@gmail.com>
2023-06-12 13:41:57 +0300

    new strings lab

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Write your code in this file!
+const currentUser = "steve austin"
+//interpolating variables always use backticks
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//.slice takes the character that is the first 
+const shortGreeting =  `Welcome, ${currentUser.slice(0,1)}!`
+
+
